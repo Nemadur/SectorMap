21ff5c2e3e8692613aeea5d7f78deb40a7378c04
Jan Piekos <jpiekos@topsolution.pl>
2020-07-23 12:55:25 +0200

    raycasting

--- js/core.js
+++ js/core.js
@@ -54,18 +54,89 @@ function core_init() {
     earth.rotation.x = 0.15;
     camera.position.z = 20;
 
+    // orbits visualization
+    var earthPath = new THREE.EllipseCurve(
+        0,  0,            // ax, aY
+        10, 10,           // xRadius, yRadius
+        0,  2 * Math.PI,  // aStartAngle, aEndAngle
+        false,            // aClockwise
+        0.3                 // aRotation
+      );
+    var moonPath = new THREE.EllipseCurve(
+        0,  0,            // ax, aY
+        2, 2,           // xRadius, yRadius
+        0,  2 * Math.PI,  // aStartAngle, aEndAngle
+        false,            // aClockwise
+        0.3                 // aRotation
+      );
+      
+    var pointsE = earthPath.getPoints( 50 );
+    var pointsM = moonPath.getPoints( 50 );
+
+    var geometryE = new THREE.BufferGeometry().setFromPoints( pointsE );
+    var geometryM = new THREE.BufferGeometry().setFromPoints( pointsM );
+
+    var material = new THREE.LineBasicMaterial( { color : 0xffffff } );
+    
+    // Create the final object to add to the scene
+    var ellipseE = new THREE.Line( geometryE, material );
+    var ellipseM = new THREE.Line( geometryM, material );
+    
+    ellipseE.rotation.x = Math.PI/2;
+    ellipseM.rotation.x = Math.PI/2;
+
+    scene.add(ellipseE);
+    earth.add(ellipseM);
+
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+    
+    function onMouseMove( event ) {
+    
+        // calculate mouse position in normalized device coordinates
+        // (-1 to +1) for both components
+    
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    }
+    
+    window.addEventListener( 'mousemove', onMouseMove, false );
+
     var update = function(){
         sun.rotation.y += 0.001;
         earth.rotation.y += 0.005;
         moon.rotation.y += 0.01;
 
-        earthOrbit.rotation.y += 0.01;
-        moonOrbit.rotation.y += 0.1;
+        earthOrbit.rotation.y += 0.005;
+        moonOrbit.rotation.y += 0.01;
     };
 
-    var render = function () {
-        renderer.render(scene, camera);
-    };
+    function render() {
+    
+        // update the picking ray with the camera and mouse position
+        raycaster.setFromCamera( mouse, camera );
+    
+        // calculate objects intersecting the picking ray
+        var intersects = raycaster.intersectObjects( scene.children, true );
+    
+        sun.material.color.set(0xffff00);
+        earth.material.color.set(0x0000ff);
+        moon.material.color.set(0xcccccc);
+
+        for (const objects of intersects) {
+
+            if (objects.object.type != 'Mesh') {
+                continue
+            }
+            objects.object.material.color.set( 0x00ff00 );
+            break;
+
+        }
+
+        renderer.render( scene, camera );
+    
+    }
 
     var loop = function () {
         requestAnimationFrame( loop );
