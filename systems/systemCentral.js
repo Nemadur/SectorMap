375aa2e9a6a31bba8f2e1deeb7c0c36dc66ea5d7
Jan Piekos <jpiekos@topsolution.pl>
2020-08-31 15:09:09 +0200

    cleanup

--- systems/systemCentral.js
+++ systems/systemCentral.js
@@ -1,19 +1,13 @@
 import { thozetis } from './022_thozetis.js';
-import { neothozetis } from '../systems/neoThozetis.js';
-import { testsystem } from '../systems/testSystem.js';
 import { ignis } from './011_ignis.js';
 
 const content = [
     {name: 'Thozetis'},
-    {name: 'NeoThozetis'},
-    {name: 'Test System'},
     {name: 'Ignis'},
 ]
 
 export {
     content,
     thozetis,
-    neothozetis,
-    testsystem,
     ignis
 };
